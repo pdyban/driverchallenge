7ec0f461e46fbca741af69460774be6ad0b7cadf
pdyban <pdyban@gmail.com>
2015-08-24 21:17:54 +0200

    Extended cross-validation to multiple features

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -41,17 +41,17 @@ def cross_validate(_features_true, _features_false, percentage):
         testFeature0 = _features_true[_from:_to]
 
         _from = _features_false.shape[0] * p*percentage
-        _to = _from + ( percentage * _features_false.shape[0] )
+        _to = _from + percentage * _features_false.shape[0]
 
         trainFeature1 = np.vstack((_features_false[0:_from], _features_false[_to:]))
         #testFeature1 = _features_false[_from:_to]
 
         # extend train feature true in order to balance against a bigger number of false features
-        trainFeature0 = np.repeat(trainFeature0, (trainFeature1.shape[0]/trainFeature0.shape[0]))[:, np.newaxis]
+        trainFeature0 = np.repeat(trainFeature0, (trainFeature1.shape[0]/trainFeature0.shape[0]), axis=0)
 
         X_train = np.vstack((trainFeature0, trainFeature1))
         X_test = testFeature0
-        Y_train = np.append(np.ones(trainFeature0.size), np.zeros(trainFeature1.size))
+        Y_train = np.append(np.ones(trainFeature0.shape[0]), np.zeros(trainFeature1.shape[0]))
         #Y_test = np.append(np.ones(testFeature0.size), np.zeros(testFeature1.size))
 
         clf = RandomForestClassifier(n_estimators=25)
@@ -87,6 +87,9 @@ if __name__ == '__main__':
 
     # for cross-validation, extend feature arrays by the width of the sliding window
     feat1_ext = np.vstack((feat1, feat1[:1000, :]))
+    feat2_ext = np.vstack((feat2, feat2[:1000, :]))
+
+    features = np.hstack((feat1_ext[:, 2, np.newaxis], feat2_ext[:, 2, np.newaxis]))
 
     # naive test: driver 1 vs driver 2
     #features_true = feat1[:200, 2, np.newaxis]
@@ -101,8 +104,8 @@ if __name__ == '__main__':
         end = (driver_index+1)*200
         end_false = (driver_index+4+1)*200
 
-        features_true = feat1_ext[begin:end, 2, np.newaxis]
-        features_false = feat1_ext[end:end_false, 2, np.newaxis]
+        features_true = features[begin:end, :]
+        features_false = features[end:end_false, :]
 
         res0, res1 = cross_validate(features_true, features_false, 0.2)
 
@@ -121,6 +124,7 @@ if __name__ == '__main__':
     # the histogram of the data
     n, bins, patches = plt.hist(res[:200, 2], normed=0, facecolor='green', alpha=0.75)
     n, bins, patches = plt.hist(res[200:400, 2], normed=0, facecolor='red', alpha=0.75)
+    n, bins, patches = plt.hist(res[400:600, 2], normed=0, facecolor='yellow', alpha=0.75)
     #plt.plot(hist[1][:-1], hist[0], 'r--', linewidth=1)
     plt.show()
 
