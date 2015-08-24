00b8b900887755135524f3f9dd93603ee1ef5fc0
pdyban <pdyban@gmail.com>
2015-08-24 19:04:11 +0200

    Configured scripts/compile_features to compile features for all driver-trip pairs as a numpy array to file

--- scripts/compile_features.py
+++ scripts/compile_features.py
@@ -1,1 +1,61 @@
 __author__ = 'missoni'
+
+"""
+Compiles list of features to files.
+"""
+
+from features import AccelerationFeature
+from features import AngleFeature
+import numpy as np
+from utils import get_trip_npy, list_all_drivers
+from utils import PATHTODRIVERDATA
+
+
+def compile_features(features):
+    """
+    for each feature, do:
+      for each driver, do:
+        for each trip, do:
+          create numpy file: driver_id, trip_id, feature_value
+
+    :param features: list of features, each a derivative of Feature type.
+    :param trips: list of trips, for each driver
+    """
+    def compile_feature(feature, fname):
+        """
+        Compiles a feature to file.
+
+        - compute value for each driver and trip
+        - store result to file
+        """
+        drivers = list_all_drivers(PATHTODRIVERDATA)[:10]
+        trips = range(1, 201)
+
+        values = np.empty((len(drivers)*len(trips), 3))
+
+        for driver_index, driver in enumerate(drivers):
+            print 'starting driver', driver_index
+            for trip_index, trip in enumerate(trips):
+                trip_points = get_trip_npy(driver, trip, PATHTODRIVERDATA)
+
+                value = feature.compute(trip_points)
+                values[driver_index*len(trips)+trip_index:] = np.array((driver, trip, value))
+
+        with open(fname, 'w') as f:
+            np.save(f, values)
+
+        with open(fname) as f:
+            assert np.load(f).all() == values.all(), "File not written correctly"
+
+    for index, feature in enumerate(features):
+        print 'computing', str(feature)
+        compile_feature(feature, '../features_npy/feat%d.npy' % index)
+
+
+if __name__ == '__main__':
+    features = [AngleFeature(0, np.mean), ]
+    features = [AccelerationFeature(10, 31, True, np.mean),
+                AccelerationFeature(30, 51, True, np.mean),
+                AccelerationFeature(30, 51, False, np.mean), ]
+
+    compile_features(features)
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,8 @@ from submission import create_complete_submission
 from local_io import write_submission_to_file
 from submission import compute_all_features
 from local_io import save_3d_plot_to_file, save_1d_plot_to_file
+from local_io import get_trip_npy, get_trip
+from local_io import list_all_drivers
+
+
+PATHTODRIVERDATA = '../drivers_npy'
